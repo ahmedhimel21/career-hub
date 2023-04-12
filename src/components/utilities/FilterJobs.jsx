3d9b6aff0a06a13e38d13535ba1af9fd0ca9269a
ahmedhimel21 <ahmedhimel000@gmail.com>
2023-04-12 11:19:32 +0600

    some issue fixed

--- src/components/utilities/FilterJobs.jsx
+++ src/components/utilities/FilterJobs.jsx
@@ -6,8 +6,7 @@ let FilterJobs = (props) =>{
   return (
     <div className="w-full bg-white px-5 py-1">
       <select name="job_type" onChange={onFilterValueChanged}>
-        <option>Filter By</option>
-        <option value="all">All</option>
+        <option value="all">Filter By</option>
         <option value="Remote">Remote</option>
         <option value="Onsite">Onsite</option>
       </select>
